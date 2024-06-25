ca7f7a302d5e41298426d5ddc7f281f190d7c65a
ShawnEdgell <milky.irl@gmail.com>
2024-06-25 16:33:19 -0400

    updated config

--- workbox-config.cjs
+++ workbox-config.cjs
@@ -4,12 +4,28 @@ module.exports = {
 	swDest: '.svelte-kit/output/client/service-worker.js',
 	runtimeCaching: [
 		{
-			urlPattern: new RegExp('^https://your-api-url.com/(.*)'),
+			// Update this URL pattern to match your actual API or your site's URL
+			urlPattern: new RegExp('^https://sveltekit-pwa-livid.vercel.app/(.*)'),
 			handler: 'NetworkFirst',
 			options: {
 				cacheName: 'api-cache',
 				expiration: {
 					maxEntries: 10
+				}
+			}
+		},
+		{
+			// Cache other requests, such as static assets
+			urlPattern: ({ request }) =>
+				request.destination === 'image' ||
+				request.destination === 'style' ||
+				request.destination === 'script',
+			handler: 'CacheFirst',
+			options: {
+				cacheName: 'asset-cache',
+				expiration: {
+					maxEntries: 50,
+					maxAgeSeconds: 30 * 24 * 60 * 60 // 30 days
 				}
 			}
 		}
